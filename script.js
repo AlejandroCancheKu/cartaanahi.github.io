e167ee6213675f248aab765f4ca81daf826c5e23
ale.dev <101384740+AlejandroCancheKu@users.noreply.github.com>
2024-02-14 14:18:01 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").hide();
+    });
+});
